1db66d2d90b5ae34d713f0f1d3d024d5cef1c9d3
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-28 19:56:57 -0700

    implement randomizeArray

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const urls = [];
 
   for (let i = 0; i < 12; i++) {
-    const randomNum = Math.floor(Math.random()*1025);
+    const randomNum = Math.ceil(Math.random()*1025);
     urls.push(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
   }
 
@@ -18,7 +18,7 @@ function App() {
     const fetchData = async () => {
       try {
         const responses = await Promise.all(urls.map(url => fetch(url).then(response => response.json())));
-        const pokemonData = responses.map(pokemon => ({ img: pokemon.sprites.front_default }));
+        const pokemonData = responses.map(pokemon => ({ img: pokemon.sprites.front_default, clicked: false }));
         setDivArray(pokemonData);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -28,49 +28,37 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(divArray);
-
-  
-  // async function fetchAPI() {
-  //   const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
-  //   const json = await response.json();
-  
-  //   return json;
-  // }
-
-  // async function fetchImage(index) {
-  //   const apiJSON = await fetchAPI();
-  //   const response = await fetch(apiJSON.results[index].url)
-  //   const json = await response.json();
+  const randomizeArray = (obj) => {
+    const updatedObj = markClicked(obj);
+    const updatedArray = divArray.map(item => (item === obj ? updatedObj : item));
+    const newArray = shuffleArray(updatedArray);
     
-  //   return json.sprites.front_default;
-  // }
+    console.log(newArray);
+    setDivArray(newArray);
+  }
 
-  // useEffect(() => {
-  //   divArray.forEach(obj => {
+  const markClicked = (obj) => {
+    return {...obj, clicked: true};
+  }
 
-  //   })
-  //   fetch('https://pokeapi.co/api/v2/pokemon/1025')
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
-  // })
+  const shuffleArray = (array) => {
+    const arrayCopy = [...array];
+    const newArray = [];
+    while (arrayCopy.length) {
+      const randomNum = Math.floor(Math.random()*arrayCopy.length);
+      newArray.push(arrayCopy.splice(randomNum, 1)[0]);
+    }
+    return newArray;
+  }
 
-  // async function setImgSrc() {
-  //   divArray.forEach((obj, index) => {
-  //     fetchImage(index).then(imgURL => {
-  //       obj.img = imgURL;
-  //     })
-  //   })
-  // }
 
-  // setImgSrc();
 
   return (
     <>
       <div>
         <div className="card-container">
           {divArray.map((obj, index) => {
-            return <div key={index} className='card'>
+            return <div key={index} className='card' onClick={() =>randomizeArray(obj)}>
               <img src={obj.img} alt="" />
             </div>
           }
